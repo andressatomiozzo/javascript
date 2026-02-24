4807249f42475f288d8aec86cdca9ea7f23d2da3
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-02-24 09:49:10 -0400

    Finalizei o exercício 301 - map, filter e reduce

---
+++ Exercicios/301-map-filter-reduce-Estatisticas-de-notas/script.js
@@ -0,0 +1,40 @@
+const nota = document.getElementById("nota");
+const addBtn = document.querySelector(".add");
+const respostaArray = document.getElementById("resposta-array");
+const calcBtn = document.querySelector(".calc");
+const resposta = document.getElementById("resposta");
+
+let dados = [];
+
+addBtn.addEventListener("click", () => {
+  dados.push(Number(nota.value));
+  respostaArray.innerText = `Array: ${dados}`;
+  nota.value = "";
+});
+
+calcBtn.addEventListener("click", () => {
+  // let maioresQue7 = dados.filter((nota) => {
+  //   if(nota > 7) return nota
+  // });
+  const maioresQue7 = dados.filter((nota) => nota > 7);
+
+  const media = dados.reduce((acc, nota) => {
+    return (acc + nota) / dados.length;
+  });
+
+  const conceito = dados.map((dado) => {
+    if (dado >= 9) {
+      return "A";
+    } else if (dado >= 8) {
+      return "B";
+    } else if (dado >= 7) {
+      return "C";
+    } else if (dado >= 6) {
+      return "D";
+    } else {
+      return "F";
+    }
+  });
+
+  resposta.innerText = `As notas maiores que 7 foram as ${maioresQue7}, a média das notas foi de ${media} e os conceitos são: ${conceito}`;
+});

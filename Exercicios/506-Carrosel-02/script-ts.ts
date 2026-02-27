d61e5d3564c6e16cd0a87f15f0ed727b2c0c9882
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-02-27 10:29:01 -0400

    Exercício 502 - Carrossel 02 - adicionei a interação com os botões

--- Exercicios/506-Carrosel-02/script-ts.ts
+++ Exercicios/506-Carrosel-02/script-ts.ts
@@ -4,8 +4,17 @@ const nextBtn = document.querySelector<HTMLButtonElement>(".next");
 
 if (!track || !prevBtn || !nextBtn) throw new Error("Algum elemento não está ligado ao DOM");
 
+const slides = Array.from(track.children);
 let indexAtual = 0;
 
 const update = () => {
   track.style.transform = `translateX(-${indexAtual * 100}%)`;
 };
+
+prevBtn.addEventListener("click", () => {
+  indexAtual = (indexAtual + slides.length - 1) % slides.length;
+});
+
+nextBtn.addEventListener("click", () => {
+  indexAtual = (indexAtual + 1) % slides.length;
+});

416f92ef1a28702558fd5a816869ea6f169056e8
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-02-27 14:33:12 -0400

    Exercício 303 - Normalização de dados - adicionei a interação com o botão 'adicionar' que adiciona os dados do usuário no array

--- Exercicios/303-Normalizacao-de-dados/script-ts.js
+++ Exercicios/303-Normalizacao-de-dados/script-ts.js
@@ -2,6 +2,19 @@
 const nomei = document.querySelector("#nome");
 const precoi = document.querySelector("#preco");
 const addBtn = document.querySelector(".add");
+const arrayContainer = document.querySelector("#array-container");
 const calcBtn = document.querySelector(".calc");
-if (!nomei || !precoi || !addBtn || !calcBtn)
+if (!nomei || !precoi || !addBtn || !arrayContainer || !calcBtn)
     throw new Error("Algum elemento não está ligado ao DOM");
+let dados = [];
+const atualizarLista = () => {
+    arrayContainer.innerHTML = dados.map((d) => `<p>Nome: ${d.nome}, preço: ${d.preco}. </p>`).join("");
+};
+addBtn.addEventListener("click", () => {
+    const nome = nomei.value;
+    const preco = Number(precoi.value);
+    dados.push({ nome, preco });
+    atualizarLista();
+    nomei.value = "";
+    precoi.value = "";
+});
--- Exercicios/303-Normalizacao-de-dados/script-ts.ts
+++ Exercicios/303-Normalizacao-de-dados/script-ts.ts
@@ -1,7 +1,25 @@
 const nomei = document.querySelector<HTMLInputElement>("#nome");
 const precoi = document.querySelector<HTMLInputElement>("#preco");
 const addBtn = document.querySelector<HTMLButtonElement>(".add");
+const arrayContainer = document.querySelector<HTMLDivElement>("#array-container");
 const calcBtn = document.querySelector<HTMLButtonElement>(".calc");
 
-if (!nomei || !precoi || !addBtn || !calcBtn) throw new Error ("Algum elemento não está ligado ao DOM");
+if (!nomei || !precoi || !addBtn || !arrayContainer || !calcBtn) throw new Error("Algum elemento não está ligado ao DOM");
 
+let dados: { nome: string; preco: number }[] = [];
+
+const atualizarLista = () => {
+  arrayContainer.innerHTML = dados.map((d) => `<p>Nome: ${d.nome}, preço: ${d.preco}. </p>`).join("");
+};
+
+addBtn.addEventListener("click", () => {
+  const nome = nomei.value;
+  const preco = Number(precoi.value);
+
+  dados.push({ nome, preco });
+
+  atualizarLista();
+
+  nomei.value = "";
+  precoi.value = "";
+});

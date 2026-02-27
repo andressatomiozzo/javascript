dd06c0a5018776e7655d433e763bbdc2e618f966
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-02-27 14:58:56 -0400

    Exercício 303 - Normalização de dados - adicionei a a interação com o botão calcular e fiz a lógica do exercício

--- Exercicios/303-Normalizacao-de-dados/script-ts.js
+++ Exercicios/303-Normalizacao-de-dados/script-ts.js
@@ -4,7 +4,8 @@ const precoi = document.querySelector("#preco");
 const addBtn = document.querySelector(".add");
 const arrayContainer = document.querySelector("#array-container");
 const calcBtn = document.querySelector(".calc");
-if (!nomei || !precoi || !addBtn || !arrayContainer || !calcBtn)
+const respostasContainer = document.querySelector("#respostas-container");
+if (!nomei || !precoi || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
     throw new Error("Algum elemento não está ligado ao DOM");
 let dados = [];
 const atualizarLista = () => {
@@ -17,4 +18,13 @@ addBtn.addEventListener("click", () => {
     atualizarLista();
     nomei.value = "";
     precoi.value = "";
+});
+calcBtn.addEventListener("click", () => {
+    const novoArray = dados.map((d) => {
+        return {
+            nome: d.nome.toLowerCase(),
+            preco: d.preco * 0.9
+        };
+    });
+    respostasContainer.innerHTML = novoArray.map((d) => `<p>Nome: ${d.nome}, preço: ${d.preco}. </p>`).join("");
 });
--- Exercicios/303-Normalizacao-de-dados/script-ts.ts
+++ Exercicios/303-Normalizacao-de-dados/script-ts.ts
@@ -3,8 +3,9 @@ const precoi = document.querySelector<HTMLInputElement>("#preco");
 const addBtn = document.querySelector<HTMLButtonElement>(".add");
 const arrayContainer = document.querySelector<HTMLDivElement>("#array-container");
 const calcBtn = document.querySelector<HTMLButtonElement>(".calc");
+const respostasContainer = document.querySelector<HTMLDivElement>("#respostas-container");
 
-if (!nomei || !precoi || !addBtn || !arrayContainer || !calcBtn) throw new Error("Algum elemento não está ligado ao DOM");
+if (!nomei || !precoi || !addBtn || !arrayContainer || !calcBtn || !respostasContainer) throw new Error("Algum elemento não está ligado ao DOM");
 
 let dados: { nome: string; preco: number }[] = [];
 
@@ -22,4 +23,15 @@ addBtn.addEventListener("click", () => {
 
   nomei.value = "";
   precoi.value = "";
+});
+
+calcBtn.addEventListener("click", () => {
+  const novoArray = dados.map((d) => {
+    return{
+      nome: d.nome.toLowerCase(),
+      preco: d.preco * 0.9
+    };
+  });
+
+  respostasContainer.innerHTML = novoArray.map((d) => `<p>Nome: ${d.nome}, preço: ${d.preco}. </p>`).join("");
 });

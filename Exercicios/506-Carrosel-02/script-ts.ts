1a536bdab2efe171bcdecf2cefb50d97e9bd1d70
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-02-27 10:27:20 -0400

    Exercício 502 - Carrossel 02 - liguei as variáveis ao DOM

--- Exercicios/506-Carrosel-02/script-ts.ts
+++ Exercicios/506-Carrosel-02/script-ts.ts
@@ -1,1 +1,11 @@
+const track = document.querySelector<HTMLDivElement>(".track");
+const prevBtn = document.querySelector<HTMLButtonElement>(".prev");
+const nextBtn = document.querySelector<HTMLButtonElement>(".next");
 
+if (!track || !prevBtn || !nextBtn) throw new Error("Algum elemento não está ligado ao DOM");
+
+let indexAtual = 0;
+
+const update = () => {
+  track.style.transform = `translateX(-${indexAtual * 100}%)`;
+};

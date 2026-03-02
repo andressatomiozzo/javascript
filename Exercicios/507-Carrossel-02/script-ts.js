0bc0d368b218a66747d0d6197146e41aebb43ffc
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-02 08:07:22 -0400

    Exercício 507 - Carrossel 02 - adicionei a função que muda os slides

--- Exercicios/507-Carrossel-02/script-ts.js
+++ Exercicios/507-Carrossel-02/script-ts.js
@@ -7,9 +7,13 @@ if (!track || !prevBtn || !nextBtn || !dotsContainer)
     throw new Error("Alguma variável não está ligado ao DOM");
 let indexAtual = 0;
 const indexTotal = Array.from(track.children);
-indexTotal.forEach((i) => {
+indexTotal.forEach((_, i) => {
     const dot = document.createElement("button");
     dot.classList.add("dots");
     dot.ariaLabel = `Ir para slide ${i}`;
     dotsContainer.appendChild(dot);
+    goTo(i);
 });
+const goTo = (i) => {
+    track.style.transform = `translateX(-${i * 100}%)`;
+};
--- Exercicios/507-Carrossel-02/script-ts.ts
+++ Exercicios/507-Carrossel-02/script-ts.ts
@@ -3,15 +3,20 @@ const prevBtn = document.querySelector<HTMLButtonElement>(".prev");
 const nextBtn = document.querySelector<HTMLButtonElement>(".next");
 const dotsContainer = document.querySelector<HTMLDivElement>("#dots-container");
 
-if (!track || !prevBtn || !nextBtn || !dotsContainer) throw new Error ("Alguma variável não está ligado ao DOM")
+if (!track || !prevBtn || !nextBtn || !dotsContainer) throw new Error("Alguma variável não está ligado ao DOM");
 
-let indexAtual = 0
+let indexAtual = 0;
 const indexTotal = Array.from(track.children);
 
+indexTotal.forEach((_, i) => {
+  const dot = document.createElement("button");
+  dot.classList.add("dots");
+  dot.ariaLabel = `Ir para slide ${i}`;
+  dotsContainer.appendChild(dot);
 
-indexTotal.forEach((i) => {
-    const dot = document.createElement("button");
-    dot.classList.add("dots");
-    dot.ariaLabel = `Ir para slide ${i}`;
-    dotsContainer.appendChild(dot);
+  goTo(i);
 });
+
+const goTo = (i: number) => {
+  track.style.transform = `translateX(-${i * 100}%)`;
+};

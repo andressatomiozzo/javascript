a88c61c37354f83c7d9cff5a2ba59687680fd9b3
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-02 11:31:27 -0400

    Exercício 108 - Remover o último elemento - fiz a lógica do exercício

--- Exercicios/108-Remover-ultimo-elemento/script-ts.js
+++ Exercicios/108-Remover-ultimo-elemento/script-ts.js
@@ -1,7 +1,8 @@
 "use strict";
 const containerCaixas = document.querySelector("#containerCaixas");
 const adicionarBtn = document.querySelector("#adicionarBtn");
-if (!containerCaixas || !adicionarBtn)
+const removerBtn = document.querySelector("#removerBtn");
+if (!containerCaixas || !adicionarBtn || !removerBtn)
     throw new Error("Algum elemento não está ligado ao DOM");
 const arrayCor = ["preto", "vermelho", "amarelo", "azul"];
 let i = 0;
@@ -10,4 +11,11 @@ adicionarBtn.addEventListener("click", () => {
     caixa.classList.add("box", `${arrayCor[i]}`);
     containerCaixas.appendChild(caixa);
     i = (i + 1) % arrayCor.length;
+});
+removerBtn.addEventListener("click", () => {
+    const ultimo = containerCaixas.lastElementChild;
+    if (!ultimo)
+        return;
+    containerCaixas.removeChild(ultimo);
+    i = (i + arrayCor.length - 1) % arrayCor.length;
 });
--- Exercicios/108-Remover-ultimo-elemento/script-ts.ts
+++ Exercicios/108-Remover-ultimo-elemento/script-ts.ts
@@ -1,14 +1,23 @@
 const containerCaixas = document.querySelector<HTMLDivElement>("#containerCaixas");
 const adicionarBtn = document.querySelector<HTMLButtonElement>("#adicionarBtn");
+const removerBtn = document.querySelector<HTMLButtonElement>("#removerBtn");
 
-if(!containerCaixas||!adicionarBtn) throw new Error("Algum elemento não está ligado ao DOM");
+if (!containerCaixas || !adicionarBtn || !removerBtn) throw new Error("Algum elemento não está ligado ao DOM");
 
-const arrayCor = ["preto", "vermelho", "amarelo", "azul"]
+const arrayCor = ["preto", "vermelho", "amarelo", "azul"];
 let i = 0;
 
 adicionarBtn.addEventListener("click", () => {
-    const caixa = document.createElement("div");
-    caixa.classList.add("box", `${arrayCor[i]}`);
-    containerCaixas.appendChild(caixa);
-    i = (i + 1) % arrayCor.length;
-})
+  const caixa = document.createElement("div");
+  caixa.classList.add("box", `${arrayCor[i]}`);
+  containerCaixas.appendChild(caixa);
+  i = (i + 1) % arrayCor.length;
+});
+
+removerBtn.addEventListener("click", () => {
+  const ultimo = containerCaixas.lastElementChild;
+  if(!ultimo) return;
+
+  containerCaixas.removeChild(ultimo);
+  i = (i + arrayCor.length - 1) % arrayCor.length;
+});

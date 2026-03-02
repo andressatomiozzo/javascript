657ef4c2054cb04e41108084a89a76bac125c379
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-02 10:09:54 -0400

    Exercício 305 - Carrinho de compras - adicionei a interação com o botão adicionar

--- Exercicios/305-Carrinho-de-compras/script-ts.js
+++ Exercicios/305-Carrinho-de-compras/script-ts.js
@@ -1,10 +1,24 @@
 "use strict";
-const produto = document.querySelector("#produto");
-const preco = document.querySelector("#preco");
-const quantidade = document.querySelector("#quantidade");
+const produtoi = document.querySelector("#produto");
+const precoi = document.querySelector("#preco");
+const quantidadei = document.querySelector("#quantidade");
 const addBtn = document.querySelector(".add");
 const arrayContainer = document.querySelector("#array-container");
 const calcBtn = document.querySelector(".calc");
 const respostasContainer = document.querySelector("#respostas-container");
-if (!produto || !preco || !quantidade || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
+if (!produtoi || !precoi || !quantidadei || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
     throw new Error("Algum elemento não está ligado ao DOM");
+let dados = [];
+addBtn.addEventListener("click", () => {
+    if (produtoi.value === "" || precoi.value === "" || quantidadei.value === "") {
+        arrayContainer.innerHTML = `<p>Preencha todos os campos corretamente</p>`;
+        arrayContainer.style.color = "red";
+    }
+    else {
+        dados.push({ produto: produtoi.value, preco: Number(precoi.value), quantidade: Number(quantidadei.value) });
+        arrayContainer.innerHTML = dados.map((d) => `<p>Produto: ${d.produto}, preço: R$${d.preco}, quantidade: ${d.quantidade}. </p>`).join("");
+        produtoi.value = "";
+        precoi.value = "";
+        quantidadei.value = "";
+    }
+});
--- Exercicios/305-Carrinho-de-compras/script-ts.ts
+++ Exercicios/305-Carrinho-de-compras/script-ts.ts
@@ -1,10 +1,27 @@
-const produto = document.querySelector<HTMLInputElement>("#produto");
-const preco = document.querySelector<HTMLInputElement>("#preco");
-const quantidade = document.querySelector<HTMLInputElement>("#quantidade");
+const produtoi = document.querySelector<HTMLInputElement>("#produto");
+const precoi = document.querySelector<HTMLInputElement>("#preco");
+const quantidadei = document.querySelector<HTMLInputElement>("#quantidade");
 const addBtn = document.querySelector<HTMLBRElement>(".add");
 const arrayContainer = document.querySelector<HTMLDivElement>("#array-container");
 const calcBtn = document.querySelector<HTMLBRElement>(".calc");
 const respostasContainer = document.querySelector<HTMLDivElement>("#respostas-container");
 
-if (!produto || !preco || !quantidade || !addBtn || !arrayContainer || !calcBtn || !respostasContainer) throw new Error("Algum elemento não está ligado ao DOM");
+if (!produtoi || !precoi || !quantidadei || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
+  throw new Error("Algum elemento não está ligado ao DOM");
 
+let dados: { produto: string; preco: number; quantidade: number }[] = [];
+
+addBtn.addEventListener("click", () => {
+  if (produtoi.value === "" || precoi.value === "" || quantidadei.value === "") {
+    arrayContainer.innerHTML = `<p>Preencha todos os campos corretamente</p>`;
+    arrayContainer.style.color = "red";
+  } else {
+    dados.push({ produto: produtoi.value, preco: Number(precoi.value), quantidade: Number(quantidadei.value) });
+
+    arrayContainer.innerHTML = dados.map((d) => `<p>Produto: ${d.produto}, preço: R$${d.preco}, quantidade: ${d.quantidade}. </p>`).join("");
+
+    produtoi.value = "";
+    precoi.value = "";
+    quantidadei.value = "";
+  }
+});

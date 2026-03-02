b3535e53fadab5b2da922cc7a5cba76c6027338c
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-02 11:13:03 -0400

    Exercício 107 - Criador de caixas - adicionei cores diferentes às caixas

--- Exercicios/107-Criador-de-caixas/script-ts.js
+++ Exercicios/107-Criador-de-caixas/script-ts.js
@@ -3,8 +3,11 @@ const containerCaixas = document.querySelector("#containerCaixas");
 const adicionarBtn = document.querySelector("#adicionarBtn");
 if (!containerCaixas || !adicionarBtn)
     throw new Error("Algum elemento não está ligado ao DOM");
+const arrayCor = ["preto", "vermelho", "amarelo", "azul"];
+let i = 0;
 adicionarBtn.addEventListener("click", () => {
     const caixa = document.createElement("div");
-    caixa.classList.add("box");
+    caixa.classList.add("box", `${arrayCor[i]}`);
     containerCaixas.appendChild(caixa);
+    i = (i + 1) % arrayCor.length;
 });
--- Exercicios/107-Criador-de-caixas/script-ts.ts
+++ Exercicios/107-Criador-de-caixas/script-ts.ts
@@ -3,8 +3,12 @@ const adicionarBtn = document.querySelector<HTMLButtonElement>("#adicionarBtn");
 
 if(!containerCaixas||!adicionarBtn) throw new Error("Algum elemento não está ligado ao DOM");
 
+const arrayCor = ["preto", "vermelho", "amarelo", "azul"]
+let i = 0;
+
 adicionarBtn.addEventListener("click", () => {
     const caixa = document.createElement("div");
-    caixa.classList.add("box");
+    caixa.classList.add("box", `${arrayCor[i]}`);
     containerCaixas.appendChild(caixa);
+    i = (i + 1) % arrayCor.length;
 })

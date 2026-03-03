fe0dfa61db02dbd84d9e034596181a22c46c4456
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-03 11:51:39 -0400

    Exercício 508 - Carrossel 02 - adicionei a função que faz o índice mudar, a função que atualiza a página pra imagem mudar e a interação com os botões

--- Exercicios/508-Carrossel-02/script-ts.js
+++ Exercicios/508-Carrossel-02/script-ts.js
@@ -6,10 +6,24 @@ const dotsContainer = document.querySelector(".dots-container");
 if (!track || !prevBtn || !nextBtn || !dotsContainer)
     throw new Error("Algum elemento não está ligado ao DOM");
 const slides = Array.from(track.children);
+let indiceAtual = 0;
 // ? ------------- Criar as bolinhas ----------------
 slides.forEach((_, i) => {
     const dot = document.createElement("div");
     dot.classList.add("dots");
     dot.ariaLabel = `Ir para a imagem ${i + 1}`;
     dotsContainer.appendChild(dot);
+    dot.addEventListener("click", () => goTo(i));
 });
+// ! =============== Função para o índice mudar =================
+const goTo = (i) => {
+    indiceAtual = (i + slides.length) % slides.length;
+    update();
+};
+// ? ============= Função que passa o slide ============
+const update = () => {
+    track.style.transform = `translateX(-${indiceAtual * 100}%)`;
+};
+// ! ------------------- Interação com os botões -----------------
+prevBtn.addEventListener("click", () => goTo(indiceAtual - 1));
+nextBtn.addEventListener("click", () => goTo(indiceAtual + 1));
--- Exercicios/508-Carrossel-02/script-ts.ts
+++ Exercicios/508-Carrossel-02/script-ts.ts
@@ -6,6 +6,7 @@ const dotsContainer = document.querySelector<HTMLDivElement>(".dots-container");
 if (!track||!prevBtn||!nextBtn||!dotsContainer) throw new Error ("Algum elemento não está ligado ao DOM");
 
 const slides = Array.from(track.children);
+let indiceAtual = 0
 
 // ? ------------- Criar as bolinhas ----------------
 
@@ -14,5 +15,23 @@ slides.forEach((_, i) => {
   dot.classList.add("dots");
   dot.ariaLabel = `Ir para a imagem ${i+1}`
   dotsContainer.appendChild(dot);
-
+  dot.addEventListener("click", () => goTo(i));
 });
+
+// ! =============== Função para o índice mudar =================
+
+const goTo = (i:number) => {
+  indiceAtual = (i + slides.length) % slides.length;
+  update();
+}
+
+// ? ============= Função que passa o slide ============
+
+const update = () => {
+  track.style.transform = `translateX(-${indiceAtual * 100}%)`
+}
+
+// ! ------------------- Interação com os botões -----------------
+
+prevBtn.addEventListener("click", () => goTo(indiceAtual - 1));
+nextBtn.addEventListener("click", () => goTo(indiceAtual + 1));

30f6b3446c5e902e6b049a95ed57d9ce6ffa8ad8
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-03 10:49:11 -0400

    Exercício 306 - Análise de dados de idade - adicionei a lógica usando apenas o .reduce

--- Exercicios/306-Analise-de-dados-de-idade/script-ts.ts
+++ Exercicios/306-Analise-de-dados-de-idade/script-ts.ts
@@ -28,23 +28,40 @@ addBtn.addEventListener("click", () => {
   }
 });
 
+// calcBtn.addEventListener("click", () => {
+//   const idades = dados.map((d) => d.idade)
+//   const somaIdades = idades.reduce((acc, d) => acc + d);
+//   const mediaIdades = somaIdades/dados.length;
+//   let maiorIdade = idades[0]
+//   idades.forEach((d) => {
+//     maiorIdade < d? maiorIdade = d : maiorIdade;
+//   })
+//   let menorIdade = idades[0];
+//   for(let i = 1; i < idades.length; i++){
+//     menorIdade > idades[i]? menorIdade = idades[i] : menorIdade
+//   }
+
+//   respostasContainer.innerHTML = `
+//     <p>A soma das idades é ${somaIdades}</p>
+//     <p>A média das idades é ${mediaIdades.toFixed(2)}</p>
+//     <p>A maior idade é ${maiorIdade}</p>
+//     <p>A menor idade é ${menorIdade}</p>
+//   `;
+// })
+
+// ! ------------ APENAS COM REDUCE ---------------
+
 calcBtn.addEventListener("click", () => {
-  const idades = dados.map((d) => d.idade)
+  const idades = dados.map((d) => d.idade);
   const somaIdades = idades.reduce((acc, d) => acc + d);
-  const mediaIdades = somaIdades/dados.length;
-  let maiorIdade = idades[0]
-  idades.forEach((d) => {
-    maiorIdade < d? maiorIdade = d : maiorIdade;
-  })
-  let menorIdade = idades[0];
-  for(let i = 1; i < idades.length; i++){
-    menorIdade > idades[i]? menorIdade = idades[i] : menorIdade
-  }
-  
+  const mediaIdades = somaIdades / idades.length;
+  const maiorIdade = idades.reduce((acc, d) => Math.max(acc, d));
+  const menorIdade = idades.reduce((acc, d) => Math.min(acc, d));
+
   respostasContainer.innerHTML = `
     <p>A soma das idades é ${somaIdades}</p>
     <p>A média das idades é ${mediaIdades.toFixed(2)}</p>
     <p>A maior idade é ${maiorIdade}</p>
     <p>A menor idade é ${menorIdade}</p>
   `;
-})
+});

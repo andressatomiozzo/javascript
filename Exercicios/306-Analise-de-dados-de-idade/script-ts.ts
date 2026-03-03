49114f5be1380e6f59e7bad1d09b02e4a851f9ca
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-03 10:35:24 -0400

    Exercício 306 - Análise de dados de idade - adicionei a interação com o botão calcular e fiz a lógica do exercício

--- Exercicios/306-Analise-de-dados-de-idade/script-ts.ts
+++ Exercicios/306-Analise-de-dados-de-idade/script-ts.ts
@@ -27,3 +27,24 @@ addBtn.addEventListener("click", () => {
     idadeInput.value = "";
   }
 });
+
+calcBtn.addEventListener("click", () => {
+  const idades = dados.map((d) => d.idade)
+  const somaIdades = idades.reduce((acc, d) => acc + d);
+  const mediaIdades = somaIdades/dados.length;
+  let maiorIdade = idades[0]
+  idades.forEach((d) => {
+    maiorIdade < d? maiorIdade = d : maiorIdade;
+  })
+  let menorIdade = idades[0];
+  for(let i = 1; i < idades.length; i++){
+    menorIdade > idades[i]? menorIdade = idades[i] : menorIdade
+  }
+  
+  respostasContainer.innerHTML = `
+    <p>A soma das idades é ${somaIdades}</p>
+    <p>A média das idades é ${mediaIdades.toFixed(2)}</p>
+    <p>A maior idade é ${maiorIdade}</p>
+    <p>A menor idade é ${menorIdade}</p>
+  `;
+})

f39075006c2bcdcf4eb62d778471b3e12d146f55
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-03 11:40:42 -0400

    Exercício 508 - Carrossel 02 - criei as bolinhas e as estilizei

--- Exercicios/508-Carrossel-02/script-ts.js
+++ Exercicios/508-Carrossel-02/script-ts.js
@@ -5,3 +5,11 @@ const nextBtn = document.querySelector(".next");
 const dotsContainer = document.querySelector(".dots-container");
 if (!track || !prevBtn || !nextBtn || !dotsContainer)
     throw new Error("Algum elemento não está ligado ao DOM");
+const slides = Array.from(track.children);
+// ? ------------- Criar as bolinhas ----------------
+slides.forEach((_, i) => {
+    const dot = document.createElement("div");
+    dot.classList.add("dots");
+    dot.ariaLabel = `Ir para a imagem ${i + 1}`;
+    dotsContainer.appendChild(dot);
+});
--- Exercicios/508-Carrossel-02/script-ts.ts
+++ Exercicios/508-Carrossel-02/script-ts.ts
@@ -4,3 +4,15 @@ const nextBtn = document.querySelector<HTMLButtonElement>(".next");
 const dotsContainer = document.querySelector<HTMLDivElement>(".dots-container");
 
 if (!track||!prevBtn||!nextBtn||!dotsContainer) throw new Error ("Algum elemento não está ligado ao DOM");
+
+const slides = Array.from(track.children);
+
+// ? ------------- Criar as bolinhas ----------------
+
+slides.forEach((_, i) => {
+  const dot = document.createElement("div");
+  dot.classList.add("dots");
+  dot.ariaLabel = `Ir para a imagem ${i+1}`
+  dotsContainer.appendChild(dot);
+
+});

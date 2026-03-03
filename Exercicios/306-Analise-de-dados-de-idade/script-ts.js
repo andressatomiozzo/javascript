18dc781b99e3227ffcd2e2051f0326a48e516b86
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-03 09:48:29 -0400

    Exercício 306 - Análise de dados de idade - adicionei a interação com o botão Adicionar para que o array aparecesse na tela e fosse guardado

--- Exercicios/306-Analise-de-dados-de-idade/script-ts.js
+++ Exercicios/306-Analise-de-dados-de-idade/script-ts.js
@@ -7,3 +7,19 @@ const calcBtn = document.querySelector(".calc");
 const respostasContainer = document.querySelector("#respostas-container");
 if (!nomeInput || !idadeInput || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
     throw new Error("Algum elemento não está ligado ao DOM");
+let dados = [];
+addBtn.addEventListener("click", () => {
+    if (nomeInput.value === "" || idadeInput.value === "" || Number(idadeInput.value) < 0 || Number(idadeInput.value) > 150) {
+        arrayContainer.innerHTML = `<p>Adicione um valor válido </p>`;
+        arrayContainer.style.color = "red";
+    }
+    else {
+        const nome = nomeInput.value;
+        const idade = Number(idadeInput.value);
+        dados.push({ nome: nome, idade: idade });
+        arrayContainer.innerHTML = `${dados.map((d) => `<p>Nome: ${d.nome}, idade: ${d.idade}</p>`).join("")}`;
+        arrayContainer.style.color = "black";
+        nomeInput.value = "";
+        idadeInput.value = "";
+    }
+});
--- Exercicios/306-Analise-de-dados-de-idade/script-ts.ts
+++ Exercicios/306-Analise-de-dados-de-idade/script-ts.ts
@@ -5,5 +5,25 @@ const arrayContainer = document.querySelector<HTMLDivElement>("#array-container"
 const calcBtn = document.querySelector<HTMLButtonElement>(".calc");
 const respostasContainer = document.querySelector<HTMLDivElement>("#respostas-container");
 
-if (!nomeInput || !idadeInput || !addBtn || !arrayContainer || !calcBtn || !respostasContainer) throw new Error("Algum elemento não está ligado ao DOM");
+if (!nomeInput || !idadeInput || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
+  throw new Error("Algum elemento não está ligado ao DOM");
 
+let dados: { nome: string; idade: number }[] = [];
+
+addBtn.addEventListener("click", () => {
+  if (nomeInput.value === "" || idadeInput.value === "" || Number(idadeInput.value) < 0 || Number(idadeInput.value) > 150) {
+    arrayContainer.innerHTML = `<p>Adicione um valor válido </p>`;
+    arrayContainer.style.color = "red";
+  } else {
+    const nome = nomeInput.value;
+    const idade = Number(idadeInput.value);
+
+    dados.push({ nome: nome, idade: idade });
+
+    arrayContainer.innerHTML = `${dados.map((d) => `<p>Nome: ${d.nome}, idade: ${d.idade}</p>`).join("")}`;
+    arrayContainer.style.color = "black";
+
+    nomeInput.value = "";
+    idadeInput.value = "";
+  }
+});

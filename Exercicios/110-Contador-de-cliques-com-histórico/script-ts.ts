33aa9d9cdbdd77548f720b715fb67b4ec0b9f491
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-03 11:12:32 -0400

    Exercício 110 - Contador de cliques - adicinei a interação e lógica com o botão adicionar um li

--- Exercicios/110-Contador-de-cliques-com-histórico/script-ts.ts
+++ Exercicios/110-Contador-de-cliques-com-histórico/script-ts.ts
@@ -5,3 +5,11 @@ const listaContainer = document.querySelector<HTMLUListElement>("#listaContainer
 if(!addBtn||!limparBtn||!listaContainer) throw new Error ("Algum elemento não está ligado ao DOM")
 
 let contador = 0
+
+addBtn.addEventListener("click", () => {
+  contador ++;
+  const li = document.createElement("li");
+  li.innerText = `${contador}`;
+  listaContainer.appendChild(li);
+})
+

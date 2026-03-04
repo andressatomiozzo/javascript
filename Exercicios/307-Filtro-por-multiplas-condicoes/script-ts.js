5235e4583dba082a5179df9350db6c8536c6f860
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-04 15:11:55 -0400

    Exercício 307 - Filtro por múltiplas condições - adicionei a interação com o botão adicionar

--- Exercicios/307-Filtro-por-multiplas-condicoes/script-ts.js
+++ Exercicios/307-Filtro-por-multiplas-condicoes/script-ts.js
@@ -1,10 +1,28 @@
 "use strict";
 const nomeInput = document.querySelector("#nome");
 const idadeInput = document.querySelector("#idade");
-const cidadeInput = document.querySelector("#cidade");
+const estadoInput = document.querySelector("#estado");
 const addBtn = document.querySelector(".add");
 const arrayContainer = document.querySelector("#array-container");
 const calcBtn = document.querySelector(".calc");
 const respostasContainer = document.querySelector("#respostas-container");
-if (!nomeInput || !idadeInput || !cidadeInput || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
+if (!nomeInput || !idadeInput || !estadoInput || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
     throw new Error("Algum elemento não está ligado ao DOM");
+let dados = [];
+addBtn.addEventListener("click", () => {
+    if (nomeInput.value === `` || idadeInput.value === `` || Number(idadeInput.value) < 0 || Number(idadeInput.value) > 130) {
+        arrayContainer.innerHTML = `<p>Escreva um valor válido <p>`;
+        arrayContainer.style.color = "red";
+    }
+    else {
+        const nome = nomeInput.value;
+        const idade = Number(idadeInput.value);
+        const estado = estadoInput.value;
+        dados.push({ nome: nome, idade: idade, estado: estado });
+        arrayContainer.innerHTML = `${dados.map((d) => `<p>Nome: ${d.nome}, idade: ${d.idade}, estado: ${d.estado}</p>`).join("")}`;
+        arrayContainer.style.color = "black";
+        nomeInput.value = "";
+        idadeInput.value = "";
+        estadoInput.value = "";
+    }
+});
--- Exercicios/307-Filtro-por-multiplas-condicoes/script-ts.ts
+++ Exercicios/307-Filtro-por-multiplas-condicoes/script-ts.ts
@@ -1,10 +1,32 @@
 const nomeInput = document.querySelector<HTMLInputElement>("#nome");
 const idadeInput = document.querySelector<HTMLInputElement>("#idade");
-const cidadeInput = document.querySelector<HTMLInputElement>("#cidade");
+const estadoInput = document.querySelector<HTMLInputElement>("#estado");
 const addBtn = document.querySelector<HTMLButtonElement>(".add");
 const arrayContainer = document.querySelector<HTMLDivElement>("#array-container");
 const calcBtn = document.querySelector<HTMLButtonElement>(".calc");
 const respostasContainer = document.querySelector<HTMLDivElement>("#respostas-container");
 
-if (!nomeInput || !idadeInput || !cidadeInput || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
+if (!nomeInput || !idadeInput || !estadoInput || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
   throw new Error("Algum elemento não está ligado ao DOM");
+
+let dados: { nome: string; idade: number; estado: string }[] = [];
+
+addBtn.addEventListener("click", () => {
+  if (nomeInput.value === `` || idadeInput.value === `` || Number(idadeInput.value) < 0 || Number(idadeInput.value) > 130) {
+    arrayContainer.innerHTML = `<p>Escreva um valor válido <p>`;
+    arrayContainer.style.color = "red";
+  } else {
+    const nome = nomeInput.value;
+    const idade = Number(idadeInput.value);
+    const estado = estadoInput.value;
+  
+    dados.push({ nome: nome, idade: idade, estado: estado });
+  
+    arrayContainer.innerHTML = `${dados.map((d) => `<p>Nome: ${d.nome}, idade: ${d.idade}, estado: ${d.estado}</p>`).join("")}`;
+    arrayContainer.style.color = "black";
+
+    nomeInput.value = "";
+    idadeInput.value = "";
+    estadoInput.value = "";
+  }
+});

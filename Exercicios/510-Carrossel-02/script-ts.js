05a78b6ebb980391caa5f74c35d5c383af66a9b7
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-06 11:26:02 -0400

    Exercício 510 - Carrossel 02 - criei as bolinhas

--- Exercicios/510-Carrossel-02/script-ts.js
+++ Exercicios/510-Carrossel-02/script-ts.js
@@ -5,3 +5,13 @@ const nextBtn = document.querySelector("#next-btn");
 const dotsCOntainer = document.querySelector("#dots-container");
 if (!track || !prevBtn || !nextBtn || !dotsCOntainer)
     throw new Error("Alguma variável não está ligada ao DOM");
+let indexAtual = 0;
+const indexTotal = Array.from(track.children);
+indexTotal.forEach((_, i) => {
+    const dot = document.createElement("button");
+    dot.ariaLabel = `Ir para a imagem ${i + 1}`;
+    dot.classList.add("dots");
+    dot.addEventListener("click", () => goTo(i));
+    dotsCOntainer.appendChild(dot);
+});
+const dots = document.querySelectorAll(".dots");
--- Exercicios/510-Carrossel-02/script-ts.ts
+++ Exercicios/510-Carrossel-02/script-ts.ts
@@ -4,3 +4,17 @@ const nextBtn = document.querySelector<HTMLButtonElement>("#next-btn");
 const dotsCOntainer = document.querySelector<HTMLDivElement>("#dots-container");
 
 if (!track || !prevBtn || !nextBtn || !dotsCOntainer) throw new Error("Alguma variável não está ligada ao DOM");
+
+let indexAtual = 0
+const indexTotal = Array.from(track.children);
+
+indexTotal.forEach((_, i) => {
+  const dot = document.createElement("button");
+  dot.ariaLabel = `Ir para a imagem ${i +1}`
+  dot.classList.add("dots");
+  dot.addEventListener("click", () => goTo(i))
+
+  dotsCOntainer.appendChild(dot);
+})
+
+const dots = document.querySelectorAll<HTMLButtonElement>(".dots");

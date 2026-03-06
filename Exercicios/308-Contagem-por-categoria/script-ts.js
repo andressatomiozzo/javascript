00ef1417a4bd979a6aadd9680377f99909475acc
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-06 09:21:39 -0400

    Exercício 308 - Contagem por categoria - adicionei a interação com o botão calcular e a lógica do exercício

--- Exercicios/308-Contagem-por-categoria/script-ts.js
+++ Exercicios/308-Contagem-por-categoria/script-ts.js
@@ -14,4 +14,18 @@ addBtn.addEventListener("click", () => {
     dados.push({ produto: produtoInput.value, categoria: categoria.value });
     console.log(dados);
     produtoInput.value = "";
+    arrayContainer.innerHTML = dados.map((d) => `<p>Produto: ${d.produto}, categoria: ${d.categoria}`).join("");
+});
+calcBtn.addEventListener("click", () => {
+    const categoria = dados.map((d) => d.categoria);
+    let eletronicos = 0;
+    let roupas = 0;
+    categoria.forEach((d) => {
+        d === "Roupas" ? roupas++ : eletronicos++;
+    });
+    const categoriaContados = { Eletronicos: eletronicos, Roupas: roupas };
+    respostasContainer.innerHTML = `
+    <p>Quantidade de eletrônicos: ${categoriaContados.Eletronicos}<p>
+    <p>Quantidade de roupas: ${categoriaContados.Roupas}<p>
+  `;
 });
--- Exercicios/308-Contagem-por-categoria/script-ts.ts
+++ Exercicios/308-Contagem-por-categoria/script-ts.ts
@@ -4,15 +4,31 @@ const arrayContainer = document.querySelector<HTMLDivElement>("#array-container"
 const calcBtn = document.querySelector<HTMLButtonElement>(".calc");
 const respostasContainer = document.querySelector<HTMLDivElement>("#respostas-container");
 
-if (!produtoInput || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
-  throw new Error("Algum elemento não está ligado ao DOM");
+if (!produtoInput || !addBtn || !arrayContainer || !calcBtn || !respostasContainer) throw new Error("Algum elemento não está ligado ao DOM");
 
 let dados: { produto: string; categoria: string }[] = [];
 
 addBtn.addEventListener("click", () => {
   const categoria = document.querySelector<HTMLInputElement>('input[name="categoria"]:checked');
   if (!categoria) throw new Error("Categoria não está ligado ao DOM");
-  dados.push({produto:produtoInput.value, categoria: categoria.value})
+  dados.push({ produto: produtoInput.value, categoria: categoria.value });
   console.log(dados);
   produtoInput.value = "";
+
+  arrayContainer.innerHTML = dados.map((d) => `<p>Produto: ${d.produto}, categoria: ${d.categoria}`).join("");
+});
+
+calcBtn.addEventListener("click", () => {
+  const categoria = dados.map((d) => d.categoria);
+  let eletronicos = 0;
+  let roupas = 0;
+  categoria.forEach((d) => {
+    d === "Roupas" ? roupas++ : eletronicos++;
+  });
+  const categoriaContados = { Eletronicos: eletronicos, Roupas: roupas };
+
+  respostasContainer.innerHTML = `
+    <p>Quantidade de eletrônicos: ${categoriaContados.Eletronicos}<p>
+    <p>Quantidade de roupas: ${categoriaContados.Roupas}<p>
+  `;
 });

824ecc292b75f91981843a725d31e9c454ecea1b
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-06 09:44:26 -0400

    Exercício 203 - Quiz de perguntas - fiz a lógica com TS

--- Exercicios/203-Quiz-de-perguntas/script-ts.js
+++ Exercicios/203-Quiz-de-perguntas/script-ts.js
@@ -8,3 +8,25 @@ const calcBtn = document.querySelector(".calc");
 const pontuacaoContent = document.querySelector("#pontuacao-content");
 if (!perguntaInput || !respostaInput || !pontosInput || !addBtn || !respostasContainer || !calcBtn || !pontuacaoContent)
     throw new Error("Algum elemento não está ligado ao DOM");
+let dados = [];
+addBtn.addEventListener("click", () => {
+    const pergunta = perguntaInput.value;
+    const resposta = respostaInput.value;
+    const pontos = Number(pontosInput.value);
+    if (pontos < 0 || pontos > 10) {
+        respostasContainer.innerHTML = `<p>Adicione um valor para pontos válido. Entre 0 a 10.</p>`;
+        respostasContainer.style.color = "red";
+    }
+    else {
+        dados.push({ pergunta: pergunta, resposta: resposta, pontos: pontos });
+        respostasContainer.innerHTML = dados.map((d) => `<p> Pergunta: ${d.pergunta}. Resposta: ${d.resposta}. Pontos: ${d.pontos}<p>`).join("");
+        respostasContainer.style.color = "black";
+        perguntaInput.value = "";
+        respostaInput.value = "";
+        pontosInput.value = "";
+    }
+});
+calcBtn.addEventListener("click", () => {
+    const pontosTotal = dados.map((d) => d.pontos).reduce((acc, d) => acc + d);
+    pontuacaoContent.innerText = `Pontuação final: ${pontosTotal}`;
+});

612d855846dec34b9756bd14be6c45730b92eb0a
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-06 10:13:32 -0400

    Exercício 204 - Classificador de números - adicionei a lógica do exercício com TS

--- Exercicios/204-Classificador-de-numeros/script-ts.js
+++ Exercicios/204-Classificador-de-numeros/script-ts.js
@@ -4,5 +4,47 @@ const addBtn = document.querySelector("#addBtn");
 const arrayContainer = document.querySelector("#array");
 const calcBtn = document.querySelector("#calcBtn");
 const resetBtn = document.querySelector("#resetBtn");
-if (!numeroInput || !addBtn || !arrayContainer || !calcBtn || !resetBtn)
+const respostaContainer = document.querySelector("#resposta-container");
+if (!numeroInput || !addBtn || !arrayContainer || !calcBtn || !resetBtn || !respostaContainer)
     throw new Error("ALgum elemento não está ligado ao DOM");
+let dados = [];
+addBtn.addEventListener("click", () => {
+    dados.push(Number(numeroInput.value));
+    arrayContainer.innerText = `Array: ${dados}`;
+    numeroInput.value = "";
+});
+calcBtn.addEventListener("click", () => {
+    let parMaior10 = [];
+    let parMenor10 = [];
+    let imparMaior10 = [];
+    let imparMenor10 = [];
+    dados.forEach((d) => {
+        if (d % 2 === 0) {
+            if (d >= 10) {
+                parMaior10.push(d);
+            }
+            else {
+                parMenor10.push(d);
+            }
+        }
+        else {
+            if (d >= 10) {
+                imparMaior10.push(d);
+            }
+            else {
+                imparMenor10.push(d);
+            }
+        }
+    });
+    respostaContainer.innerHTML = `
+    <p>Números pares menores que 10: ${parMenor10.length === 0 ? `nenhum` : parMenor10}</p>
+    <p>Números pares maiores ou igual a 10: ${parMaior10.length === 0 ? `nenhum` : parMaior10}</p>
+    <p>Números ímpares menores que 10: ${imparMenor10.length === 0 ? `nenhum` : imparMenor10}</p>
+    <p>Números ímpares maiores ou igual a 10: ${imparMaior10.length === 0 ? `nenhum` : imparMaior10}</p>
+  `;
+});
+resetBtn.addEventListener("click", () => {
+    dados = [];
+    arrayContainer.innerText = "Array: ";
+    respostaContainer.innerHTML = "";
+});
--- Exercicios/204-Classificador-de-numeros/script-ts.ts
+++ Exercicios/204-Classificador-de-numeros/script-ts.ts
@@ -3,5 +3,49 @@ const addBtn = document.querySelector<HTMLButtonElement>("#addBtn");
 const arrayContainer = document.querySelector<HTMLParagraphElement>("#array");
 const calcBtn = document.querySelector<HTMLButtonElement>("#calcBtn");
 const resetBtn = document.querySelector<HTMLButtonElement>("#resetBtn");
+const respostaContainer = document.querySelector<HTMLDivElement>("#resposta-container");
 
-if (!numeroInput || !addBtn || !arrayContainer || !calcBtn || !resetBtn) throw new Error("ALgum elemento não está ligado ao DOM");
+if (!numeroInput || !addBtn || !arrayContainer || !calcBtn || !resetBtn || !respostaContainer)
+  throw new Error("ALgum elemento não está ligado ao DOM");
+
+let dados: number[] = [];
+
+addBtn.addEventListener("click", () => {
+  dados.push(Number(numeroInput.value));
+  arrayContainer.innerText = `Array: ${dados}`;
+  numeroInput.value = "";
+});
+
+calcBtn.addEventListener("click", () => {
+  let parMaior10: number[] = [];
+  let parMenor10: number[] = [];
+  let imparMaior10: number[] = [];
+  let imparMenor10: number[] = [];
+  dados.forEach((d) => {
+    if (d % 2 === 0) {
+      if (d >= 10) {
+        parMaior10.push(d);
+      } else {
+        parMenor10.push(d);
+      }
+    } else {
+      if (d >= 10) {
+        imparMaior10.push(d);
+      } else {
+        imparMenor10.push(d);
+      }
+    }
+  });
+  respostaContainer.innerHTML = `
+    <p>Números pares menores que 10: ${parMenor10.length === 0? `nenhum` : parMenor10}</p>
+    <p>Números pares maiores ou igual a 10: ${parMaior10.length === 0? `nenhum` : parMaior10}</p>
+    <p>Números ímpares menores que 10: ${imparMenor10.length === 0? `nenhum` : imparMenor10}</p>
+    <p>Números ímpares maiores ou igual a 10: ${imparMaior10.length === 0? `nenhum` : imparMaior10}</p>
+  `;
+});
+
+resetBtn.addEventListener("click", () => {
+  dados = [];
+  arrayContainer.innerText = "Array: "
+  respostaContainer.innerHTML = ""
+})

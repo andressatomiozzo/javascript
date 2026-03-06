6583c501fe8434487214f0b5955110471f39663a
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-06 09:12:58 -0400

    Exercício 308 - Contagem por categoria - adicionei a interação com o botão de adicionar

--- Exercicios/308-Contagem-por-categoria/script-ts.js
+++ Exercicios/308-Contagem-por-categoria/script-ts.js
@@ -1,10 +1,17 @@
 "use strict";
 const produtoInput = document.querySelector("#produto");
-const eletroInput = document.querySelector("#eletro");
-const roupaInput = document.querySelector("#roupa");
 const addBtn = document.querySelector(".add");
 const arrayContainer = document.querySelector("#array-container");
 const calcBtn = document.querySelector(".calc");
 const respostasContainer = document.querySelector("#respostas-container");
-if (!produtoInput || !eletroInput || !roupaInput || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
+if (!produtoInput || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
     throw new Error("Algum elemento não está ligado ao DOM");
+let dados = [];
+addBtn.addEventListener("click", () => {
+    const categoria = document.querySelector('input[name="categoria"]:checked');
+    if (!categoria)
+        throw new Error("Categoria não está ligado ao DOM");
+    dados.push({ produto: produtoInput.value, categoria: categoria.value });
+    console.log(dados);
+    produtoInput.value = "";
+});
--- Exercicios/308-Contagem-por-categoria/script-ts.ts
+++ Exercicios/308-Contagem-por-categoria/script-ts.ts
@@ -1,11 +1,18 @@
 const produtoInput = document.querySelector<HTMLInputElement>("#produto");
-const eletroInput = document.querySelector<HTMLInputElement>("#eletro");
-const roupaInput = document.querySelector<HTMLInputElement>("#roupa");
 const addBtn = document.querySelector<HTMLButtonElement>(".add");
 const arrayContainer = document.querySelector<HTMLDivElement>("#array-container");
 const calcBtn = document.querySelector<HTMLButtonElement>(".calc");
 const respostasContainer = document.querySelector<HTMLDivElement>("#respostas-container");
 
-if (!produtoInput || !eletroInput || !roupaInput || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
+if (!produtoInput || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
   throw new Error("Algum elemento não está ligado ao DOM");
 
+let dados: { produto: string; categoria: string }[] = [];
+
+addBtn.addEventListener("click", () => {
+  const categoria = document.querySelector<HTMLInputElement>('input[name="categoria"]:checked');
+  if (!categoria) throw new Error("Categoria não está ligado ao DOM");
+  dados.push({produto:produtoInput.value, categoria: categoria.value})
+  console.log(dados);
+  produtoInput.value = "";
+});

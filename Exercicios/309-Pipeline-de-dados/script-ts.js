e6757e3d0f07e490830a4bbb989c915f2660b6e6
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-07 09:26:34 -0400

    Exercício 309 - Pipeline de dados - fiz a lógica do exercício

--- Exercicios/309-Pipeline-de-dados/script-ts.js
+++ Exercicios/309-Pipeline-de-dados/script-ts.js
@@ -3,7 +3,22 @@ const numerosInput = document.querySelector("#numeros");
 const addBtn = document.querySelector(".add");
 const calcBtn = document.querySelector(".calc");
 const resetBtn = document.querySelector(".reset");
-const arrayContainer = document.querySelector("#array-container");
+const arrayContent = document.querySelector("#array-content");
 const respostaContent = document.querySelector("#resposta-content");
-if (!numerosInput || !addBtn || !calcBtn || !resetBtn || !arrayContainer || !respostaContent)
+if (!numerosInput || !addBtn || !calcBtn || !resetBtn || !arrayContent || !respostaContent)
     throw new Error("Algum elemento não está ligado ao DOM");
+let numerosArray = [];
+addBtn.addEventListener("click", () => {
+    numerosArray.push(Number(numerosInput.value));
+    arrayContent.innerText = `Array: ${numerosArray}`;
+    numerosInput.value = "";
+});
+calcBtn.addEventListener("click", () => {
+    const resposta = numerosArray.filter((n) => n % 2 === 0).map((n) => n * 2).reduce((acc, n) => acc + n);
+    respostaContent.innerText = `${resposta}`;
+});
+resetBtn.addEventListener("click", () => {
+    numerosArray = [];
+    arrayContent.innerText = "";
+    respostaContent.innerText = "";
+});
--- Exercicios/309-Pipeline-de-dados/script-ts.ts
+++ Exercicios/309-Pipeline-de-dados/script-ts.ts
@@ -2,8 +2,27 @@ const numerosInput = document.querySelector<HTMLInputElement>("#numeros");
 const addBtn = document.querySelector<HTMLButtonElement>(".add");
 const calcBtn = document.querySelector<HTMLButtonElement>(".calc");
 const resetBtn = document.querySelector<HTMLButtonElement>(".reset");
-const arrayContainer = document.querySelector<HTMLDivElement>("#array-container");
+const arrayContent = document.querySelector<HTMLParagraphElement>("#array-content");
 const respostaContent = document.querySelector<HTMLParagraphElement>("#resposta-content");
 
-if (!numerosInput || !addBtn || !calcBtn || !resetBtn || !arrayContainer || !respostaContent)
+if (!numerosInput || !addBtn || !calcBtn || !resetBtn || !arrayContent || !respostaContent)
   throw new Error("Algum elemento não está ligado ao DOM");
+
+let numerosArray: number[] = [];
+
+addBtn.addEventListener("click", () => {
+  numerosArray.push(Number(numerosInput.value));
+  arrayContent.innerText = `Array: ${numerosArray}`;
+  numerosInput.value = ""
+})
+
+calcBtn.addEventListener("click", () => {
+  const resposta = numerosArray.filter((n) => n % 2 === 0).map((n) => n * 2).reduce((acc, n) => acc + n);
+  respostaContent.innerText = `${resposta}`
+})
+
+resetBtn.addEventListener("click", () => {
+  numerosArray = [];
+  arrayContent.innerText = "";
+  respostaContent.innerText = "";
+})

bba6ddc1455573eec34b959a1ae7d97f79491b54
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-09 11:21:40 -0400

    Exercício 511 - Carrossel 03 - adicionei as features do carrossel 02

--- Exercicios/511-Carrossel-03/script-ts.js
+++ Exercicios/511-Carrossel-03/script-ts.js
@@ -5,3 +5,28 @@ const nextBtn = document.querySelector("#next-btn");
 const dotsContainer = document.querySelector("#dots-container");
 if (!track || !prevBtn || !nextBtn || !dotsContainer)
     throw new Error("Algum elemento não está ligado ao DOM");
+let indexAtual = 0;
+const indexTotal = Array.from(track.children);
+indexTotal.forEach((_, i) => {
+    const dot = document.createElement("button");
+    dot.classList.add("dots");
+    dot.ariaLabel = `Ir para a imagem ${i + 1}`;
+    dot.addEventListener("click", () => goTo(i));
+    dotsContainer.appendChild(dot);
+});
+const dots = document.querySelectorAll(".dots");
+const update = () => {
+    track.style.transform = `translateX(-${indexAtual * 100}%)`;
+    dots.forEach((dot, i) => {
+        dot.classList.remove("dots-active");
+        if (i === indexAtual)
+            dot.classList.add("dots-active");
+    });
+};
+const goTo = (o) => {
+    indexAtual = (o + indexTotal.length) % indexTotal.length;
+    update();
+};
+prevBtn.addEventListener("click", () => goTo(indexAtual - 1));
+nextBtn.addEventListener("click", () => goTo(indexAtual + 1));
+update();
--- Exercicios/511-Carrossel-03/script-ts.ts
+++ Exercicios/511-Carrossel-03/script-ts.ts
@@ -5,3 +5,35 @@ const dotsContainer = document.querySelector<HTMLDivElement>("#dots-container");
 
 if (!track || !prevBtn || !nextBtn || !dotsContainer) throw new Error("Algum elemento não está ligado ao DOM");
 
+let indexAtual = 0;
+const indexTotal = Array.from(track.children);
+
+indexTotal.forEach((_, i) => {
+  const dot = document.createElement("button");
+  dot.classList.add("dots");
+  dot.ariaLabel = `Ir para a imagem ${i + 1}`;
+  dot.addEventListener("click", () => goTo(i));
+
+  dotsContainer.appendChild(dot);
+});
+
+const dots = document.querySelectorAll<HTMLButtonElement>(".dots");
+
+const update = () => {
+  track.style.transform = `translateX(-${indexAtual * 100}%)`;
+
+  dots.forEach((dot, i) => {
+    dot.classList.remove("dots-active");
+    if (i === indexAtual) dot.classList.add("dots-active");
+  });
+};
+
+const goTo = (o: number) => {
+  indexAtual = (o + indexTotal.length) % indexTotal.length;
+  update();
+};
+
+prevBtn.addEventListener("click", () => goTo(indexAtual - 1));
+nextBtn.addEventListener("click", () => goTo(indexAtual + 1));
+
+update();

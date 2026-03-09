a1186b4107b2476e3f232fd4d5fbbf5d06f338fc
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-09 09:33:00 -0400

    Exercício 310 - Relatório final - adicionei a interação com o botão calcular e fiz as lógicas do exercício

--- Exercicios/310-Relatorio-final/script-ts.js
+++ Exercicios/310-Relatorio-final/script-ts.js
@@ -26,9 +26,19 @@ addBtn.addEventListener("click", () => {
         else {
             dados.push({ nome: nome, salario: salario, ativo: false });
         }
-        arrayContainer.innerHTML = `${dados.map((d) => `<p>Nome: ${d.nome}, salário: R$${d.salario.toFixed(2)}, ativo: ${d.ativo === true ? "pago" : "em atrazo"}</p>`).join("")}`;
+        arrayContainer.innerHTML = `${dados.map((d) => `<p>Nome: ${d.nome}, salário: R$${d.salario.toFixed(2)}, situação: ${d.ativo === true ? "ativo" : "desligado"}</p>`).join("")}`;
         arrayContainer.style.color = "black";
         nomeInput.value = "";
         salarioInput.value = "";
     }
+});
+calcBtn.addEventListener("click", () => {
+    const funcAtivos = dados.filter((d) => d.ativo === true);
+    const totalGasto = funcAtivos.map((d) => d.salario).reduce((acc, d) => acc + d);
+    const media = totalGasto / funcAtivos.length;
+    const final = { total: totalGasto, media: media };
+    const funcAtivosNomes = funcAtivos.map((d) => d.nome);
+    console.log(final);
+    respostasContainer.innerHTML = `<p>Total gasto: ${final.total}. Média: ${final.media.toFixed(2)}. </p>
+  <p>Funcionários ativos: ${funcAtivosNomes}.</p>`;
 });

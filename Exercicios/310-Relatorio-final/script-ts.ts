c50f48f92e506b529d4edd12c3b8b3fc14243330
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-09 09:22:27 -0400

    Exercício 310 - Relatório final - adicionei a interação com o botão adicionar

--- Exercicios/310-Relatorio-final/script-ts.ts
+++ Exercicios/310-Relatorio-final/script-ts.ts
@@ -8,3 +8,31 @@ const respostasContainer = document.querySelector<HTMLDivElement>("#respostas-co
 if (!nomeInput || !salarioInput || !addBtn || !arrayContainer || !calcBtn || !respostasContainer)
   throw new Error("Algum elemento não está ligado ao DOM");
 
+let dados: { nome: string; salario: number; ativo: boolean }[] = [];
+
+addBtn.addEventListener("click", () => {
+  const ativoInput = document.querySelector<HTMLSelectElement>("#ativo");
+  if (!ativoInput) throw new Error("Nenhum ativo está marcado");
+
+  const nome = nomeInput.value;
+  const salario = Number(salarioInput.value);
+  const ativo = ativoInput.value;
+
+  if (nome === "" || salario < 0 || ativo === "") {
+    arrayContainer.innerHTML = `Adicione um valor válido`;
+    arrayContainer.style.color = "red";
+  } else {
+    if (ativo === "t") {
+      dados.push({ nome: nome, salario: salario, ativo: true });
+    } else {
+      dados.push({ nome: nome, salario: salario, ativo: false });
+    }
+    arrayContainer.innerHTML = `${dados.map((d) => `<p>Nome: ${d.nome}, salário: R$${d.salario.toFixed(2)}, ativo: ${d.ativo === true ? "pago" : "em atrazo"}</p>`).join("")}`;
+    arrayContainer.style.color = "black";
+
+    nomeInput.value = "";
+    salarioInput.value = "";
+  }
+});
+
+

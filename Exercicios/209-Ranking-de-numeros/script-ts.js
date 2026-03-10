3cb6116b3b6b08ef5f2f80e47a29837c8a77c560
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-10 12:03:56 -0400

    Exercício 209 - Ranking de números - atualizei para TS

---
+++ Exercicios/209-Ranking-de-numeros/script-ts.js
@@ -0,0 +1,36 @@
+"use strict";
+const numeroInput = document.querySelector("#numeros");
+const addBtn = document.querySelector("#add-btn");
+const arrayContainer = document.querySelector("#resposta-do-array");
+const maiorBtn = document.querySelector("#maior-valor-btn");
+const menorBtn = document.querySelector("#menor-valor-btn");
+const maiorContainer = document.querySelector("#resposta-do-maior");
+const menorContainer = document.querySelector("#resposta-do-menor");
+if (!numeroInput || !addBtn || !arrayContainer || !maiorBtn || !menorBtn || !maiorContainer || !menorContainer)
+    throw new Error("Algum elemento não está ligado ao DOM");
+let dados = [];
+addBtn.addEventListener("click", () => {
+    if (numeroInput.value === "") {
+        arrayContainer.textContent = "Digite um valor válido";
+    }
+    else {
+        dados.push(Number(numeroInput.value));
+        arrayContainer.textContent = `Array: ${dados}`;
+    }
+});
+maiorBtn.addEventListener("click", () => {
+    let maior = dados[0];
+    dados.forEach((d) => {
+        if (d > maior)
+            maior = d;
+    });
+    maiorContainer.innerText = `Maior número: ${maior}`;
+});
+menorBtn.addEventListener("click", () => {
+    let menor = dados[0];
+    for (let i = 1; i < dados.length; i++) {
+        if (dados[i] < menor)
+            menor = dados[i];
+    }
+    menorContainer.innerText = `Menor número: ${menor}`;
+});

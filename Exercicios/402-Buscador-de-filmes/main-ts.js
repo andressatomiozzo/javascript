2f5657410a6412cb40d2f4131adf0f9122f38815
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-12 09:47:13 -0400

    Exercício 402 - Buscador de filmes - peguei a chave que o usuário adicionou e mandei em uma função assíncrona pra fazer o fecth

--- Exercicios/402-Buscador-de-filmes/main-ts.js
+++ Exercicios/402-Buscador-de-filmes/main-ts.js
@@ -6,3 +6,27 @@ const resetBtn = document.querySelector(".reset");
 const respostaContainer = document.querySelector("#resposta-container");
 if (!tituloInput || !anoInput || !pesqBtn || !resetBtn || !respostaContainer)
     throw new Error("Algum elemento não está ligado ao DOM");
+const pesquisarFilme = async (chave) => {
+    const apiKey = "a226e844";
+    const resposta = await fetch(`http://www.omdbapi.com/?${chave}&apikey=${apiKey}`);
+    const dados = await resposta.json();
+    console.log(dados);
+};
+pesqBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (tituloInput.value === "") {
+        respostaContainer.innerHTML = `<p>Digite um título</p>`;
+    }
+    else {
+        let chave;
+        const title = tituloInput.value;
+        const tituloQuebrado = title.split(" ").join("+");
+        if (anoInput.value == "") {
+            chave = `t=${tituloQuebrado}`;
+        }
+        else {
+            chave = `t=${tituloQuebrado}&y=${anoInput.value}`;
+        }
+        pesquisarFilme(chave);
+    }
+});
--- Exercicios/402-Buscador-de-filmes/main-ts.ts
+++ Exercicios/402-Buscador-de-filmes/main-ts.ts
@@ -6,3 +6,27 @@ const respostaContainer = document.querySelector<HTMLDivElement>("#resposta-cont
 
 if (!tituloInput || !anoInput || !pesqBtn || !resetBtn || !respostaContainer) throw new Error("Algum elemento não está ligado ao DOM");
 
+const pesquisarFilme = async (chave: string) => {
+  const apiKey = "a226e844"
+  const resposta = await fetch(`http://www.omdbapi.com/?${chave}&apikey=${apiKey}`);
+  const dados = await resposta.json();
+  console.log(dados);
+};
+
+pesqBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (tituloInput.value === "") {
+    respostaContainer.innerHTML = `<p>Digite um título</p>`;
+  } else {
+    let chave: string;
+    const title = tituloInput.value;
+    const tituloQuebrado = title.split(" ").join("+");
+    if (anoInput.value == "") {
+      chave = `t=${tituloQuebrado}`;
+    } else {
+      chave = `t=${tituloQuebrado}&y=${anoInput.value}`;
+    }
+    pesquisarFilme(chave);
+  }
+});

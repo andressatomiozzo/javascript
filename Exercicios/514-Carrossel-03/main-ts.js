2e69c9c281c3b48ca9fe56f1c95dead788547ac3
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-13 11:01:03 -0400

    Exercício 514 - Carrossel 03 - fiz o carrossel 02

---
+++ Exercicios/514-Carrossel-03/main-ts.js
@@ -0,0 +1,33 @@
+"use strict";
+const track = document.querySelector("#track");
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
+const dotsCOntainer = document.querySelector("#dots-container");
+if (!track || !prevBtn || !nextBtn || !dotsCOntainer)
+    throw new Error("Alguma variável não está ligada ao DOM");
+let indexAtual = 0;
+const indexTotal = Array.from(track.children);
+indexTotal.forEach((_, i) => {
+    const dot = document.createElement("button");
+    dot.ariaLabel = `Ir para a imagem ${i + 1}`;
+    dot.classList.add("dots");
+    dot.addEventListener("click", () => goTo(i));
+    dotsCOntainer.appendChild(dot);
+});
+const dots = document.querySelectorAll(".dots");
+const update = () => {
+    track.style.transform = `translateX(-${indexAtual * 100}%)`;
+    dots.forEach((dot, i) => {
+        dot.classList.remove("dots-active");
+        if (i === indexAtual) {
+            dot.classList.add("dots-active");
+        }
+    });
+};
+const goTo = (i) => {
+    indexAtual = (i + indexTotal.length) % indexTotal.length;
+    update();
+};
+prevBtn.addEventListener("click", () => goTo(indexAtual - 1));
+nextBtn.addEventListener("click", () => goTo(indexAtual + 1));
+update();

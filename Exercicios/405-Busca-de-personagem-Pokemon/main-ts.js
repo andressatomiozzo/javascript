38f19616b490479cd378e889f59a4a38d6594707
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-15 12:33:25 -0400

    Exercício 405 - Busca de personagem Pokémon - fiz a funão que reseta os valores

--- Exercicios/405-Busca-de-personagem-Pokemon/main-ts.js
+++ Exercicios/405-Busca-de-personagem-Pokemon/main-ts.js
@@ -2,21 +2,32 @@
 const nomeInput = document.querySelector("#nome");
 const form = document.querySelector("form");
 const alerta = document.querySelector("#alerta");
+const respostaFoto = document.querySelector("#resposta-foto");
 const respostaNome = document.querySelector("#resposta-nome");
 const respostaAltura = document.querySelector("#resposta-altura");
 const respostaPeso = document.querySelector("#resposta-peso");
-if (!nomeInput || !form || !alerta || !respostaNome || !respostaAltura || !respostaPeso)
+if (!nomeInput || !form || !alerta || !respostaFoto || !respostaNome || !respostaAltura || !respostaPeso)
     throw new Error("Algum elemento não está ligado ao DOM");
+const resetar = () => {
+    respostaNome.innerHTML = "";
+    respostaAltura.innerText = "";
+    respostaPeso.innerText = "";
+    respostaFoto.src = "";
+    alerta.innerText = "";
+};
 const mostrarNaTela = (dados) => {
-    respostaNome.innerText = `Nome: ${dados.name}`;
+    resetar();
+    respostaNome.innerHTML = `Nome: <span>${dados.name}<span>`;
     respostaAltura.innerText = `Altura: ${dados.height * 10}cm`;
     respostaPeso.innerText = `Peso: ${dados.weight / 10}kg`;
+    respostaFoto.src = `${dados.sprites.front_default}`;
 };
 const buscarPokemon = async (nome) => {
     try {
         const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`);
         const dados = await resposta.json();
         if (!resposta.ok) {
+            resetar();
             alerta.innerText = `Houve algum problena com a API.`;
         }
         else {
@@ -24,6 +35,7 @@ const buscarPokemon = async (nome) => {
         }
     }
     catch (err) {
+        resetar();
         alerta.innerText = `Houve algum problena com a API, verifique se o nome digitado está correto`;
         console.log(err);
     }
--- Exercicios/405-Busca-de-personagem-Pokemon/main-ts.ts
+++ Exercicios/405-Busca-de-personagem-Pokemon/main-ts.ts
@@ -1,22 +1,37 @@
 const nomeInput = document.querySelector<HTMLInputElement>("#nome");
 const form = document.querySelector<HTMLButtonElement>("form");
 const alerta = document.querySelector<HTMLParagraphElement>("#alerta");
+const respostaFoto = document.querySelector<HTMLImageElement>("#resposta-foto");
 const respostaNome = document.querySelector<HTMLParagraphElement>("#resposta-nome");
 const respostaAltura = document.querySelector<HTMLParagraphElement>("#resposta-altura");
 const respostaPeso = document.querySelector<HTMLParagraphElement>("#resposta-peso");
 
-if (!nomeInput || !form || !alerta || !respostaNome || !respostaAltura || !respostaPeso) throw new Error("Algum elemento não está ligado ao DOM");
+if (!nomeInput || !form || !alerta || !respostaFoto || !respostaNome || !respostaAltura || !respostaPeso)
+  throw new Error("Algum elemento não está ligado ao DOM");
+
+const resetar = () => {
+  respostaNome.innerHTML = "";
+  respostaAltura.innerText = "";
+  respostaPeso.innerText = "";
+  respostaFoto.src = "";
+  alerta.innerText = "";
+};
 
 type data = {
   name: string;
   height: number;
   weight: number;
+  sprites: {
+    front_default: string;
+  };
 };
 
 const mostrarNaTela = (dados: data) => {
-  respostaNome.innerText = `Nome: ${dados.name}`;
-  respostaAltura.innerText = `Altura: ${dados.height*10}cm`;
-  respostaPeso.innerText = `Peso: ${dados.weight/10}kg`;
+  resetar();
+  respostaNome.innerHTML = `Nome: <span>${dados.name}<span>`;
+  respostaAltura.innerText = `Altura: ${dados.height * 10}cm`;
+  respostaPeso.innerText = `Peso: ${dados.weight / 10}kg`;
+  respostaFoto.src = `${dados.sprites.front_default}`;
 };
 
 const buscarPokemon = async (nome: string) => {
@@ -24,11 +39,13 @@ const buscarPokemon = async (nome: string) => {
     const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`);
     const dados = await resposta.json();
     if (!resposta.ok) {
+      resetar();
       alerta.innerText = `Houve algum problena com a API.`;
     } else {
       mostrarNaTela(dados);
     }
   } catch (err) {
+    resetar();
     alerta.innerText = `Houve algum problena com a API, verifique se o nome digitado está correto`;
     console.log(err);
   }

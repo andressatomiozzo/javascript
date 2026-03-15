5365a30c7ca14d1c6610646e77c7e25c39c4db52
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-15 12:05:51 -0400

    Exercício 405 - Busca de personagem Pokémon - fiz a busca na API

--- Exercicios/405-Busca-de-personagem-Pokemon/main-ts.js
+++ Exercicios/405-Busca-de-personagem-Pokemon/main-ts.js
@@ -1,9 +1,24 @@
 "use strict";
 const nomeInput = document.querySelector("#nome");
-const detalhesBtn = document.querySelector(".detalhes");
+const form = document.querySelector("form");
+const alerta = document.querySelector("#alerta");
 const respostaFoto = document.querySelector("#resposta-foto");
 const respostaNome = document.querySelector("#resposta-nome");
 const respostaAltura = document.querySelector("#resposta-altura");
-const respostaoPeso = document.querySelector("#respostao-peso");
-if (!nomeInput || !detalhesBtn || !respostaFoto || !respostaNome || !respostaAltura || !respostaoPeso)
+const respostaPeso = document.querySelector("#resposta-peso");
+if (!nomeInput || !form || !alerta || !respostaFoto || !respostaNome || !respostaAltura || !respostaPeso)
     throw new Error("Algum elemento não está ligado ao DOM");
+const buscarPokemon = async (nome) => {
+    const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`);
+    const dados = await resposta.json();
+    console.log(dados);
+};
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (!nomeInput.value) {
+        alerta.innerText = "Digite algum nome de Pokémon";
+    }
+    else {
+        buscarPokemon(nomeInput.value);
+    }
+});
--- Exercicios/405-Busca-de-personagem-Pokemon/main-ts.ts
+++ Exercicios/405-Busca-de-personagem-Pokemon/main-ts.ts
@@ -1,10 +1,26 @@
 const nomeInput = document.querySelector<HTMLInputElement>("#nome");
-const detalhesBtn = document.querySelector<HTMLButtonElement>(".detalhes");
+const form = document.querySelector<HTMLButtonElement>("form");
+const alerta = document.querySelector<HTMLParagraphElement>("#alerta");
 const respostaFoto = document.querySelector<HTMLParagraphElement>("#resposta-foto");
 const respostaNome = document.querySelector<HTMLParagraphElement>("#resposta-nome");
 const respostaAltura = document.querySelector<HTMLParagraphElement>("#resposta-altura");
-const respostaoPeso = document.querySelector<HTMLParagraphElement>("#respostao-peso");
+const respostaPeso = document.querySelector<HTMLParagraphElement>("#resposta-peso");
 
-if (!nomeInput || !detalhesBtn || !respostaFoto || !respostaNome || !respostaAltura || !respostaoPeso)
+if (!nomeInput || !form || !alerta || !respostaFoto || !respostaNome || !respostaAltura || !respostaPeso)
   throw new Error("Algum elemento não está ligado ao DOM");
 
+const buscarPokemon = async (nome:string) => {
+  const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`);
+  const dados = await resposta.json();
+  console.log(dados);
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (!nomeInput.value) {
+    alerta.innerText = "Digite algum nome de Pokémon"
+  } else {
+    buscarPokemon(nomeInput.value);
+  }
+
+});

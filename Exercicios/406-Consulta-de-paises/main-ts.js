ff743e3e9380ff84e90fc0b0a8a21a33c2dac8cc
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-17 14:06:43 -0400

    Exercício 406 - Consulta de países - manipulei a API para aparecer os resultados na tela

--- Exercicios/406-Consulta-de-paises/main-ts.js
+++ Exercicios/406-Consulta-de-paises/main-ts.js
@@ -20,14 +20,40 @@ if (!nomeInput ||
     !respostaContinente ||
     !respostaLingua)
     throw new Error("Ops, algum elemento não está ligado ao DOM");
+const resetarResposta = () => {
+    respostaBandeira.src = "";
+    respostaMaps.href = "";
+    respostaMaps.innerText = "";
+    respostaNomeOficial.innerText = "";
+    respostaNomeNativo.innerText = "";
+    respostaCapital.innerText = "";
+    respostaContinente.innerText = "";
+    respostaLingua.innerText = "";
+};
+const mostrarNaTela = (dados) => {
+    alerta.innerText = "";
+    nomeInput.value = "";
+    respostaBandeira.src = `${dados[0].flags.png ?? "Sem bandeira"}`;
+    respostaMaps.href = `${dados[0].maps.googleMaps ?? "#"}`;
+    respostaMaps.innerText = "Clique para ver no Mapa!";
+    respostaNomeOficial.innerText = `Nome oficial: ${dados[0].name.official}`;
+    respostaNomeNativo.innerText = `Nome nativo: ${Object.values(dados[0].name.nativeName ?? {})
+        .map((d) => d.official)
+        .join(", ") ?? "Sem nome nativo"}`;
+    respostaCapital.innerText = `Capital: ${dados[0].capital ?? "Sem capital"}`;
+    respostaContinente.innerText = `Continente: ${dados[0].continents ?? "Sem continente"}`;
+    respostaLingua.innerText = `Línguas: ${Object.values(dados[0].languages) ?? "Sem línguas faladas"}`;
+};
 const buscarPaises = async (pais) => {
     try {
         const resposta = await fetch(`https://restcountries.com/v3.1/name/${pais}`);
         const dados = await resposta.json();
+        console.log(dados);
         mostrarNaTela(dados);
     }
     catch (err) {
         alerta.innerText = `Houve algum problema, verifique a ortografia.`;
+        resetarResposta();
         console.log(err);
     }
 };
--- Exercicios/406-Consulta-de-paises/main-ts.ts
+++ Exercicios/406-Consulta-de-paises/main-ts.ts
@@ -23,13 +23,62 @@ if (
 )
   throw new Error("Ops, algum elemento não está ligado ao DOM");
 
+const resetarResposta = () => {
+  respostaBandeira.src = "";
+  respostaMaps.href = "";
+  respostaMaps.innerText = "";
+  respostaNomeOficial.innerText = "";
+  respostaNomeNativo.innerText = "";
+  respostaCapital.innerText = "";
+  respostaContinente.innerText = "";
+  respostaLingua.innerText = "";
+};
+
+type data = [
+  {
+    name: {
+      official: string;
+      nativeName: Record<                    //um objeto com chaves desconhecidas mas valores do tipo string: objeto
+        string,
+        {
+          official: string;
+          common: string;
+        }
+      >;
+    };
+    altSpellings: string[];
+    languages: Record<string, string>;
+    capital: string[];
+    continents: string[];
+    flags: { png: string };
+    maps: { googleMaps: string };
+  },
+];
+
+const mostrarNaTela = (dados: data) => {
+  alerta.innerText = "";
+  nomeInput.value = "";
+  respostaBandeira.src = `${dados[0].flags.png ?? "Sem bandeira"}`;
+  respostaMaps.href = `${dados[0].maps.googleMaps ?? "#"}` ;
+  respostaMaps.innerText = "Clique para ver no Mapa!";
+  respostaNomeOficial.innerText = `Nome oficial: ${dados[0].name.official}`;
+  respostaNomeNativo.innerText = `Nome nativo: ${Object.values(dados[0].name.nativeName ?? {})
+    .map((d) => d.official)
+    .join(", ") ?? "Sem nome nativo"}`;
+  respostaCapital.innerText = `Capital: ${dados[0].capital  ?? "Sem capital"}`;
+  respostaContinente.innerText = `Continente: ${dados[0].continents  ?? "Sem continente"}`;
+  respostaLingua.innerText = `Línguas: ${Object.values(dados[0].languages) ?? "Sem línguas faladas"}`;
+};
+
 const buscarPaises = async (pais: string) => {
   try {
     const resposta = await fetch(`https://restcountries.com/v3.1/name/${pais}`);
     const dados = await resposta.json();
+    console.log(dados)
     mostrarNaTela(dados);
   } catch (err) {
     alerta.innerText = `Houve algum problema, verifique a ortografia.`;
+    resetarResposta();
     console.log(err);
   }
 };

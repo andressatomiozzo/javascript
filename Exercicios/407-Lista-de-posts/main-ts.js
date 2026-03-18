5af67a69490bebcae23ed31b8a7c5591383c4da5
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-18 18:16:00 -0400

    Exercício 407 - Lista de posts- mostrei a resposta na tela

--- Exercicios/407-Lista-de-posts/main-ts.js
+++ Exercicios/407-Lista-de-posts/main-ts.js
@@ -2,13 +2,38 @@
 const postsQuantidadeInput = document.querySelector("#posts-quantidade");
 const form = document.querySelector("form");
 const alerta = document.querySelector("#alerta");
-const respostaContainer = document.querySelector("#posts-quantidade");
+const respostaContainer = document.querySelector("#resposta-container");
 if (!postsQuantidadeInput || !form || !alerta || !respostaContainer)
     throw new Error("Algum elemento não está ligado ao DOM");
-const buscarPosts = async (postsQuantidade) => {
-    const resposta = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${postsQuantidade}`);
-    const dados = await resposta.json();
+const mostrarResposta = (dados) => {
     console.log(dados);
+    let soma = 1;
+    dados.forEach((d) => {
+        const divDados = document.createElement("div");
+        const idDados = document.createElement("p");
+        const titleDados = document.createElement("p");
+        const bodyDados = document.createElement("p");
+        idDados.innerText = `Post ${d.id}`;
+        titleDados.innerText = `Título: ${d.title}`;
+        bodyDados.innerText = `Corpo: ${d.body}`;
+        divDados.appendChild(idDados);
+        divDados.appendChild(titleDados);
+        divDados.appendChild(bodyDados);
+        respostaContainer.appendChild(divDados);
+        soma = soma + 1;
+        console.log(soma);
+    });
+};
+const buscarPosts = async (postsQuantidade) => {
+    try {
+        const resposta = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${postsQuantidade}`);
+        const dados = await resposta.json();
+        mostrarResposta(dados);
+    }
+    catch (err) {
+        console.log(err);
+        alerta.innerText = "Houve algum erro na busca";
+    }
 };
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
--- Exercicios/407-Lista-de-posts/main-ts.ts
+++ Exercicios/407-Lista-de-posts/main-ts.ts
@@ -1,14 +1,51 @@
 const postsQuantidadeInput = document.querySelector<HTMLInputElement>("#posts-quantidade");
 const form = document.querySelector<HTMLButtonElement>("form");
 const alerta = document.querySelector<HTMLParagraphElement>("#alerta");
-const respostaContainer = document.querySelector<HTMLDivElement>("#posts-quantidade");
+const respostaContainer = document.querySelector<HTMLDivElement>("#resposta-container");
 
 if (!postsQuantidadeInput || !form || !alerta || !respostaContainer) throw new Error("Algum elemento não está ligado ao DOM");
 
-const buscarPosts = async (postsQuantidade: number) => {
-  const resposta = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${postsQuantidade}`);
-  const dados = await resposta.json();
+type data = [
+  {
+    userId: string;
+    id: string;
+    title: string;
+    body: string;
+  },
+];
+
+const mostrarResposta = (dados: data) => {
   console.log(dados);
+  let soma = 1;
+  dados.forEach((d) => {
+    const divDados = document.createElement("div");
+    const idDados = document.createElement("p");
+    const titleDados = document.createElement("p");
+    const bodyDados = document.createElement("p");
+
+    idDados.innerText = `Post ${d.id}`;
+    titleDados.innerText = `Título: ${d.title}`;
+    bodyDados.innerText = `Corpo: ${d.body}`;
+
+    divDados.appendChild(idDados);
+    divDados.appendChild(titleDados);
+    divDados.appendChild(bodyDados);
+    respostaContainer.appendChild(divDados);
+
+    soma = soma + 1;
+    console.log(soma);
+  });
+};
+
+const buscarPosts = async (postsQuantidade: number) => {
+  try {
+    const resposta = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${postsQuantidade}`);
+    const dados = await resposta.json();
+    mostrarResposta(dados);
+  } catch (err) {
+    console.log(err);
+    alerta.innerText = "Houve algum erro na busca";
+  }
 };
 
 form.addEventListener("submit", (evento) => {

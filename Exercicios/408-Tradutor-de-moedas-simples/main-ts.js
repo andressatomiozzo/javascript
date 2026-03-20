9120a81a3a925b38cd7a6658a9223e1e5a4d61b3
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-20 10:14:06 -0400

    Exercício 408 - Tradutor de moedas simples - busquei os dados da API

--- Exercicios/408-Tradutor-de-moedas-simples/main-ts.js
+++ Exercicios/408-Tradutor-de-moedas-simples/main-ts.js
@@ -2,6 +2,32 @@
 const valorInput = document.querySelector("#valor");
 const tipoEntradaInput = document.querySelector("#tipo-entrada");
 const tipoSaidaInput = document.querySelector("#tipo-saida");
-const converterBtn = document.querySelector(".converter");
-if (!valorInput || !tipoEntradaInput || !tipoSaidaInput || !converterBtn)
+const form = document.querySelector("form");
+const alerta = document.querySelector("#alerta");
+if (!valorInput || !tipoEntradaInput || !tipoSaidaInput || !form || !alerta)
     throw new Error("Algum elemento não está ligado ao DOM");
+const converterValor = async () => {
+    try {
+        const resposta = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${tipoEntradaInput.value}.json`);
+        if (!resposta.ok) {
+            alerta.innerText = "Houve algum problema na conversão, tente mais tarde";
+        }
+        else {
+            const dados = await resposta.json();
+            console.log(resposta);
+            console.log(dados);
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (tipoEntradaInput.value === "" || tipoSaidaInput.value === "") {
+        alerta.innerText = "Selecione a moeda de troca";
+    }
+    else {
+        converterValor();
+    }
+});
--- Exercicios/408-Tradutor-de-moedas-simples/main-ts.ts
+++ Exercicios/408-Tradutor-de-moedas-simples/main-ts.ts
@@ -1,7 +1,31 @@
 const valorInput = document.querySelector<HTMLInputElement>("#valor");
 const tipoEntradaInput = document.querySelector<HTMLSelectElement>("#tipo-entrada");
 const tipoSaidaInput = document.querySelector<HTMLSelectElement>("#tipo-saida");
-const converterBtn = document.querySelector<HTMLButtonElement>(".converter");
+const form = document.querySelector<HTMLFormElement>("form");
+const alerta = document.querySelector<HTMLParagraphElement>("#alerta");
 
-if (!valorInput || !tipoEntradaInput || !tipoSaidaInput || !converterBtn) throw new Error("Algum elemento não está ligado ao DOM");
+if (!valorInput || !tipoEntradaInput || !tipoSaidaInput || !form || !alerta) throw new Error("Algum elemento não está ligado ao DOM");
 
+const converterValor = async () => {
+  try {
+    const resposta = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${tipoEntradaInput.value}.json`);
+    if(!resposta.ok) {
+      alerta.innerText = "Houve algum problema na conversão, tente mais tarde";
+    } else {
+      const dados = await resposta.json();
+      console.log(resposta)
+      console.log(dados)
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (tipoEntradaInput.value === ""|| tipoSaidaInput.value === "") {
+    alerta.innerText = "Selecione a moeda de troca"
+  } else {
+    converterValor();
+  }
+});

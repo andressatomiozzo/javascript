cae953c83b635c49ab1a12eec48be020e03ec9e8
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-20 15:54:00 -0400

    Exercício 517 - Carrossel 04 - adicionei a lógica que distribui as classes entre as imagens e as estilizei

--- Exercicios/517-Carrossel-04/main-ts.js
+++ Exercicios/517-Carrossel-04/main-ts.js
@@ -1,1 +1,46 @@
 "use strict";
+const carrossel = document.querySelector("#carrossel");
+const carrosselImg = document.querySelectorAll(".carrossel-img");
+const dotsContainer = document.querySelector("#dots-container");
+if (!carrossel || !dotsContainer)
+    throw new Error("Alguma variável não está ligada ao DOM");
+let indexAtual = 0;
+const indexTotal = Array.from(carrossel.children);
+let autoplayId;
+indexTotal.forEach((_, i) => {
+    const dot = document.createElement("button");
+    dot.ariaLabel = `Ir para a imagem ${i + 1}`;
+    dot.classList.add("dots");
+    dot.addEventListener("click", () => goTo(i));
+    dotsContainer.appendChild(dot);
+});
+const dots = document.querySelectorAll(".dots");
+const update = () => {
+    carrosselImg.forEach((img, i) => {
+        img.classList.remove("active", "prev", "next", "hidden");
+        if (i === (indexAtual + indexTotal.length - 1) % indexTotal.length) {
+            img.classList.add("prev");
+        }
+        else if (i === indexAtual) {
+            img.classList.add("active");
+        }
+        else if (i === (indexAtual + indexTotal.length + 1) % indexTotal.length) {
+            img.classList.add("next");
+        }
+        else {
+            img.classList.add("hidden");
+        }
+        img.addEventListener("click", () => goTo(i));
+    });
+    dots.forEach((dot, i) => {
+        dot.classList.remove("dots-active");
+        if (i === indexAtual) {
+            dot.classList.add("dots-active");
+        }
+    });
+};
+const goTo = (i) => {
+    indexAtual = (i + indexTotal.length) % indexTotal.length;
+    update();
+};
+update();
---
+++ Exercicios/517-Carrossel-04/main-ts.ts
@@ -0,0 +1,50 @@
+const carrossel = document.querySelector<HTMLDivElement>("#carrossel");
+const carrosselImg = document.querySelectorAll<HTMLImageElement>(".carrossel-img");
+const dotsContainer = document.querySelector<HTMLDivElement>("#dots-container");
+
+if (!carrossel || !dotsContainer) throw new Error("Alguma variável não está ligada ao DOM");
+
+let indexAtual = 0
+const indexTotal = Array.from(carrossel.children);
+let autoplayId:number;
+
+indexTotal.forEach((_, i) => {
+  const dot = document.createElement("button");
+  dot.ariaLabel = `Ir para a imagem ${i +1}`
+  dot.classList.add("dots");
+  dot.addEventListener("click", () => goTo(i))
+
+  dotsContainer.appendChild(dot);
+})
+
+const dots = document.querySelectorAll<HTMLButtonElement>(".dots");
+
+const update = () => {
+  carrosselImg.forEach((img, i) => {
+    img.classList.remove("active", "prev", "next", "hidden");
+    if (i === (indexAtual + indexTotal.length - 1) % indexTotal.length) {
+      img.classList.add("prev");
+    } else if(i === indexAtual) {
+      img.classList.add("active");
+    } else if (i === (indexAtual + indexTotal.length + 1) % indexTotal.length) {
+      img.classList.add("next");
+    } else {
+      img.classList.add("hidden");
+    }
+    img.addEventListener("click", () => goTo(i))
+  })
+
+  dots.forEach((dot, i) => {
+    dot.classList.remove("dots-active");
+    if(i === indexAtual) {
+      dot.classList.add("dots-active")
+    }
+  })
+}
+
+const goTo = (i:number) => {
+  indexAtual = (i + indexTotal.length) % indexTotal.length
+  update();
+}
+
+update();

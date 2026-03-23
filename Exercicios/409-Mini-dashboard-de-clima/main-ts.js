c6b7193764d8f4060fd1cef0db98a8d5da3bf65e
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-23 18:33:21 -0400

    Exercício 409 - Miini dashboard de clima - busquei os dados da API com o método GET

--- Exercicios/409-Mini-dashboard-de-clima/main-ts.js
+++ Exercicios/409-Mini-dashboard-de-clima/main-ts.js
@@ -5,5 +5,48 @@ const lonInput = document.querySelector("#lon");
 const form = document.querySelector("form");
 const alerta = document.querySelector("#alerta");
 const respostaContainer = document.querySelector("#resposta-container");
-if (!cidadeInput || !latInput || !lonInput || !form || !alerta || !respostaContainer)
+const tempo = document.querySelector("#tempo");
+const temperatura = document.querySelector("#temperatura");
+const sensacaoTermica = document.querySelector("#sensacao-termica");
+const tempMin = document.querySelector("#temp-min");
+const tempMax = document.querySelector("#temp-max");
+const umidade = document.querySelector("#umidade");
+if (!cidadeInput ||
+    !latInput ||
+    !lonInput ||
+    !form ||
+    !alerta ||
+    !respostaContainer ||
+    !tempo ||
+    !temperatura ||
+    !sensacaoTermica ||
+    !tempMax ||
+    !tempMin ||
+    !umidade)
     throw new Error("Alguma variável não está ligada ao DOM");
+const minhaKey = `af0c221d7e46cecd00f11fbb9eb17c63`;
+const buscarClimaCidade = async (cidade) => {
+    try {
+        const resposta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${minhaKey}`);
+        if (!resposta.ok) {
+            alerta.innerText = "Algo deu errado, verifique a ortografia";
+        }
+        else {
+            const dados = await resposta.json();
+            console.log(dados);
+        }
+    }
+    catch (err) {
+        console.log(err);
+        alerta.innerText = "Algo deu errado, verifique a ortografia";
+    }
+};
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // if (cidadeInput.value === "" && latInput.value !== "" && lonInput.value !== "") {
+    //   buscarClimaLatLon(Number(latInput.value), Number(lonInput.value));
+    // } else
+    if (cidadeInput.value !== "" && latInput.value === "" && lonInput.value === "") {
+        buscarClimaCidade(cidadeInput.value);
+    }
+});
--- Exercicios/409-Mini-dashboard-de-clima/main-ts.ts
+++ Exercicios/409-Mini-dashboard-de-clima/main-ts.ts
@@ -5,5 +5,53 @@ const form = document.querySelector<HTMLFormElement>("form");
 
 const alerta = document.querySelector<HTMLParagraphElement>("#alerta");
 const respostaContainer = document.querySelector<HTMLDivElement>("#resposta-container");
+const tempo = document.querySelector<HTMLParagraphElement>("#tempo");
+const temperatura = document.querySelector<HTMLParagraphElement>("#temperatura");
+const sensacaoTermica = document.querySelector<HTMLParagraphElement>("#sensacao-termica");
+const tempMin = document.querySelector<HTMLParagraphElement>("#temp-min");
+const tempMax = document.querySelector<HTMLParagraphElement>("#temp-max");
+const umidade = document.querySelector<HTMLParagraphElement>("#umidade");
 
-if (!cidadeInput || !latInput || !lonInput || !form || !alerta || !respostaContainer) throw new Error("Alguma variável não está ligada ao DOM");
+if (
+  !cidadeInput ||
+  !latInput ||
+  !lonInput ||
+  !form ||
+  !alerta ||
+  !respostaContainer ||
+  !tempo ||
+  !temperatura ||
+  !sensacaoTermica ||
+  !tempMax ||
+  !tempMin ||
+  !umidade
+)
+  throw new Error("Alguma variável não está ligada ao DOM");
+
+const minhaKey = `af0c221d7e46cecd00f11fbb9eb17c63`;
+
+const buscarClimaCidade = async (cidade: string) => {
+  try {
+    const resposta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${minhaKey}`);
+    if (!resposta.ok) {
+      alerta.innerText = "Algo deu errado, verifique a ortografia";
+    } else {
+      const dados = await resposta.json();
+      console.log(dados);
+    }
+  } catch (err) {
+    console.log(err);
+    alerta.innerText = "Algo deu errado, verifique a ortografia";
+  }
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // if (cidadeInput.value === "" && latInput.value !== "" && lonInput.value !== "") {
+  //   buscarClimaLatLon(Number(latInput.value), Number(lonInput.value));
+  // } else
+  if (cidadeInput.value !== "" && latInput.value === "" && lonInput.value === "") {
+    buscarClimaCidade(cidadeInput.value);
+  }
+});

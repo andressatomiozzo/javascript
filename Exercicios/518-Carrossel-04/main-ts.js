402522fdf30ba4d42a34f9e37286d6c302592c30
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-24 10:49:38 -0400

    Exercício 518 - Carrossel 04 - adicionei a função que atualiza as classes das imagens

--- Exercicios/518-Carrossel-04/main-ts.js
+++ Exercicios/518-Carrossel-04/main-ts.js
@@ -5,16 +5,34 @@ const dotsContainer = document.querySelector(".dots-container");
 if (!carrossel || !carrosselImg || !dotsContainer)
     throw new Error("Algum elemento não está ligado ao DOM");
 let indexAtual = 0;
-const indexTotal = Array.from(carrossel.children);
-indexTotal.forEach((_, i) => {
+const indexTotal = carrosselImg.length;
+carrosselImg.forEach((img, i) => {
     const dot = document.createElement("button");
     dot.classList.add("dot");
     dot.ariaLabel = `Ir para a imagem ${i + 1}`;
     dot.addEventListener("click", () => goTo(i));
+    img.addEventListener("click", () => goTo(i));
     dotsContainer.appendChild(dot);
 });
 const dots = document.querySelectorAll(".dot");
-const update = () => {
+const updateSlides = () => {
+    carrosselImg.forEach((img, i) => {
+        img.classList.remove("prev", "active", "next", "hidden");
+        if (i === (indexAtual - 1 + indexTotal) % indexTotal) {
+            img.classList.add("prev");
+        }
+        else if (i === indexAtual) {
+            img.classList.add("active");
+        }
+        else if (i === (indexAtual + 1 + indexTotal) % indexTotal) {
+            img.classList.add("next");
+        }
+        else {
+            img.classList.add("hidden");
+        }
+    });
+};
+const updateDots = () => {
     dots.forEach((dot, i) => {
         dot.classList.remove("dot-active");
         if (i === indexAtual) {
@@ -22,7 +40,12 @@ const update = () => {
         }
     });
 };
+const update = () => {
+    updateDots();
+    updateSlides();
+};
 const goTo = (index) => {
-    indexAtual = (index + indexTotal.length) % indexTotal.length;
+    indexAtual = (index + indexTotal) % indexTotal;
     update();
 };
+update();

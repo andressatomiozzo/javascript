6d93511369187b97fd2525d389e06d9bc3cecab3
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-03-24 10:37:35 -0400

    Exercício 518 - Carrossel 04 - adicionei a função que atualiza o index e a que atualiza os dots ativos

--- Exercicios/518-Carrossel-04/main-ts.js
+++ Exercicios/518-Carrossel-04/main-ts.js
@@ -10,5 +10,19 @@ indexTotal.forEach((_, i) => {
     const dot = document.createElement("button");
     dot.classList.add("dot");
     dot.ariaLabel = `Ir para a imagem ${i + 1}`;
+    dot.addEventListener("click", () => goTo(i));
     dotsContainer.appendChild(dot);
 });
+const dots = document.querySelectorAll(".dot");
+const update = () => {
+    dots.forEach((dot, i) => {
+        dot.classList.remove("dot-active");
+        if (i === indexAtual) {
+            dot.classList.add("dot-active");
+        }
+    });
+};
+const goTo = (index) => {
+    indexAtual = (index + indexTotal.length) % indexTotal.length;
+    update();
+};
--- Exercicios/518-Carrossel-04/main-ts.ts
+++ Exercicios/518-Carrossel-04/main-ts.ts
@@ -11,5 +11,23 @@ indexTotal.forEach((_, i) => {
   const dot = document.createElement("button");
   dot.classList.add("dot");
   dot.ariaLabel = `Ir para a imagem ${i + 1}`;
+  dot.addEventListener("click", () => goTo(i));
+
   dotsContainer.appendChild(dot);
 });
+
+const dots = document.querySelectorAll(".dot");
+
+const update = () => {
+  dots.forEach((dot, i) => {
+    dot.classList.remove("dot-active");
+    if (i === indexAtual) {
+      dot.classList.add("dot-active");
+    }
+  });
+};
+
+const goTo = (index: number) => {
+  indexAtual = (index + indexTotal.length) % indexTotal.length;
+  update();
+};

2f46ef4c048b0a9fa3755b94ce55d2d88015ee49
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-04-01 17:47:03 -0400

    Exercício 412 - Criar comentário - fiz a lógica e mandei os dados pelo método POST para a API

--- Exercicios/412-Criar-comentario/main-ts.js
+++ Exercicios/412-Criar-comentario/main-ts.js
@@ -6,3 +6,29 @@ const alerta = document.querySelector("#alerta");
 const respostaContainer = document.querySelector("#resposta-container");
 if (!nomeInput || !mensagemInput || !form || !alerta || !respostaContainer)
     throw new Error("Algum elemento não está ligado ao DOM!");
+const enviarComentario = async () => {
+    try {
+        const resposta = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit`, {
+            method: "POST",
+            headers: { "Content-type": "aplication/json" },
+            body: JSON.stringify({
+                name: nomeInput.value,
+                comment: mensagemInput.value,
+            }),
+        });
+        const dados = await resposta.json();
+        if (!resposta.ok) {
+            throw new Error(dados);
+        }
+        console.log(`Dados enviados: Nome: ${nomeInput.value}, Comentário: ${mensagemInput.value}`);
+        nomeInput.value = "";
+        mensagemInput.value = "";
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    enviarComentario();
+});
